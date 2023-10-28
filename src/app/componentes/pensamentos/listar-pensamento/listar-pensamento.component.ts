eb5dd84088cc267bee893c0ef6e31549854c2b50
lucas silva <lucassilva.ferre@gmail.com>
2023-10-28 16:20:26 -0300

    Adicionando backend json server

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -6,24 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent {
-  listaPensamentos = [
-    {
-      conteudo: "Passo informações para componente filho",
-      autoria: "Componente pai",
-      modelo: "modelo1"
-    },
-    {
-      conteudo: "Minha propriedade é decorada com @Input",
-      autoria: "Componente filho",
-      modelo: "modelo2"
-    },
-    {
-      conteudo: "Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor " +
-      "ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi." +
-      "ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi.",
-      autoria: "",
-      modelo: "modelo3"
-    }
+  listaPensamentos : Pensamento[] = [
     // Ctrl + K + C comentar linhas
     // Ctrl + k + u descomentar
   ];
